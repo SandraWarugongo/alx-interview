7c304c94be900614547b0034208a55f6013a1a75
SandraWarugongo <sandra.wwarugongo@gmail.com>
2024-08-13 12:49:32 -0700

    changes made

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,13 @@
 #!/usr/bin/python3
-"""Write a script that reads stdin line by line and computes metrics:
-
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size> (if the format is not this one, the line
-must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size>
-(see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer,
-don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
-
-line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
-<status code>, <file size>]
+# -*- coding: utf-8 -*-
 """
+Created on Thur July 20 15:00:00 2023
 
-
+@Author: Nicanor Kyamba
+"""
 import sys
 
-# store the count of all status codes in a dictionary
+
 status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                      '404': 0, '405': 0, '500': 0}
 
@@ -38,27 +22,22 @@ try:
             status_code = line_list[-2]
             file_size = int(line_list[-1])
 
-            # check if the status code receive exists in the dictionary and
-            # increment its count
             if status_code in status_codes_dict.keys():
                 status_codes_dict[status_code] += 1
 
-            # update total size
             total_size += file_size
 
-            # update count of lines
             count += 1
 
         if count == 10:
-            count = 0  # reset count
+            count = 0
             print('File size: {}'.format(total_size))
 
-            # print out status code counts
             for key, value in sorted(status_codes_dict.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
-except Exception as err:
+except Exception as error:
     pass
 
 finally:
