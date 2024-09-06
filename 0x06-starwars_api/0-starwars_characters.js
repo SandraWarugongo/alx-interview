b2c66aa8477fac47d38314c54372fed72055fa48
sandra warugongo <sandra.wwarugongo@gmail.com>
2024-09-06 04:07:45 +0300

    the sun shines bright

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,58 +1,36 @@
 #!/usr/bin/node
-
-const request = require('request');
-
-// Retrieve movie ID from command line argument
-// const movieId = process.argv[2];
+const argv = process.argv;
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+//const urlMovie = `${urlFilm}${argv[2]}/`;
 //
-// if (!movieId) {
-//   console.error('Please provide a movie ID as a command line argument.');
-//     process.exit(1);
-//     }
+//const request = require('request');
 //
-//     // Make request to get movie details
-//     request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}`, (error, response, body) => {
-//       if (error) {
-//           console.error('Error making API request:', error);
-//               process.exit(1);
-//                 }
+//request(urlMovie, function (error, response, body) {
+//  if (error == null) {
+//      const fbody = JSON.parse(body);
+//          const characters = fbody.characters;
 //
-//                   if (response.statusCode !== 200) {
-//                       console.error('API request failed with status code:', response.statusCode);
-//                           process.exit(1);
-//                             }
+//              if (characters && characters.length > 0) {
+//                    const limit = characters.length;
+//                          CharRequest(0, characters[0], characters, limit);
+//                              }
+//                                } else {
+//                                    console.log(error);
+//                                      }
+//                                      });
 //
-//                               const movie = JSON.parse(body);
-//
-//                                 // Fetch each character in the movie
-//                                   const characters = movie.characters;
-//                                     const characterPromises = characters.map((characterUrl) => {
-//                                         return new Promise((resolve, reject) => {
-//                                               request.get(characterUrl, (error, response, body) => {
-//                                                       if (error) {
-//                                                                 reject(error);
-//                                                                         }
-//
-//                                                                                 if (response.statusCode !== 200) {
-//                                                                                           reject(new Error(`API request failed with status code: ${response.statusCode}`));
-//                                                                                                   }
-//
-//                                                                                                           const character = JSON.parse(body);
-//                                                                                                                   resolve(character.name);
-//                                                                                                                         });
-//                                                                                                                             });
-//                                                                                                                               });
-//
-//                                                                                                                                 // Print characters once all promises are resolved
-//                                                                                                                                   Promise.all(characterPromises)
-//                                                                                                                                       .then((characterNames) => {
-//                                                                                                                                             characterNames.forEach((name) => {
-//                                                                                                                                                     console.log(name);
-//                                                                                                                                                           });
-//                                                                                                                                                               })
-//                                                                                                                                                                   .catch((error) => {
-//                                                                                                                                                                         console.error('Error fetching character details:', error);
-//                                                                                                                                                                               process.exit(1);
-//                                                                                                                                                                                   });
-//                                                                                                                                                                                   });
-//
+//                                      function CharRequest (idx, url, characters, limit) {
+//                                        if (idx === limit) {
+//                                            return;
+//                                              }
+//                                                request(url, function (error, response, body) {
+//                                                    if (!error) {
+//                                                          const rbody = JSON.parse(body);
+//                                                                console.log(rbody.name);
+//                                                                      idx++;
+//                                                                            CharRequest(idx, characters[idx], characters, limit);
+//                                                                                } else {
+//                                                                                      console.error('error:', error);
+//                                                                                          }
+//                                                                                            });
+//                                                                                            }
